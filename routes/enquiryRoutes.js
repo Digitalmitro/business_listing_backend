6751d47e89a3919f78608f0639ae834ff6ec1c7c
soumendigitalmitro <soumen.digitalmitro@gmail.com>
2025-03-27 17:48:59 +0530

    fix bugs

--- routes/enquiryRoutes.js
+++ routes/enquiryRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const {createEnquiry, getAllEnquiry} = require('../controllers/enquiryController')
+const {createEnquiry, getAllEnquiry, resloveEnquiry} = require('../controllers/enquiryController')
 router.post('/enquiry', createEnquiry)
 router.get('/enquiry', getAllEnquiry);
-router.put("/enquiry/:id",)
+router.put("/enquiry/:id",resloveEnquiry)
 
 module.exports = router;
